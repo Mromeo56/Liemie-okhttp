a2d2b4d22e0e19bc942ba00b3642beaeab24f0fb
laugere <martin@jsutmagic.com>
2020-03-21 19:05:31 +0100

    instantiate Visite class

--- app/src/main/java/com/example/liemie/Visite.java
+++ app/src/main/java/com/example/liemie/Visite.java
@@ -1,4 +1,27 @@
 package com.example.liemie;
 
+import java.util.Date;
+
 public class Visite {
+    private int idPatient;
+    private Date date;
+    private int duree;
+
+    public Visite(int unIdPatient, Date uneDate, int uneDuree) {
+        this.idPatient = unIdPatient;
+        this.date = uneDate;
+        this.duree = uneDuree;
+    }
+
+    public int getIdPatient() {
+        return this.idPatient;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public int getDuree() {
+        return this.duree;
+    }
 }
