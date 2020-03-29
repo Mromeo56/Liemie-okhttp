a067faa1fdc6d16835095e67e1b9d5bc2f692199
laugere <martin@jsutmagic.com>
2020-03-29 13:35:40 +0200

    Change Http function

--- app/src/main/java/com/example/liemie/http/Http.java
+++ app/src/main/java/com/example/liemie/http/Http.java
@@ -19,13 +19,14 @@ public class Http {
     private Methode methode;
     private String cookie;
     private Response response;
+    private Request.Builder request;
 
 
     public Http() {
         this.client = new OkHttpClient();
         this.JSON = MediaType.parse("application/json; charset=utf-8");
         this.methode = Methode.GET;
-
+        this.request = new Request.Builder();
     }
 
     public Http setMethode(Methode methode) {
@@ -34,47 +35,44 @@ public class Http {
     }
 
     public Http addUrl(String url) {
-        this.url = url;
+        this.request.url(url);
         return this;
     }
 
     public Http addBody(String data) {
-        this.body = RequestBody.create(JSON, data);
+        switch (this.methode) {
+            case POST:
+                this.request.post(RequestBody.create(JSON, data));
+                break;
+            case PUT:
+                this.request.put(RequestBody.create(JSON, data));
+                break;
+            case DELETE:
+                this.request.delete(RequestBody.create(JSON, data));
+                break;
+            default:
+                break;
+        }
         return this;
     }
 
     public Http addCookie(String data) {
+        this.addHeader("Set-Cookie", data);
         return this;
     }
 
-    public Http addHeader(String header) {
+    public Http addHeader(String name, String value) {
+        this.request.addHeader(name, value);
         return this;
     }
 
     public Request build() {
-        Request request = null;
-
-        switch (this.methode) {
-            case POST:
-                request = new Request.Builder()
-                        .url(this.url)
-                        .post(this.body)
-                        .build();
-                break;
-            case PUT:
-
-                break;
-            case DELETE:
-
-                break;
-            default:
-                request = new Request.Builder()
-                        .url(this.url)
-                        .build();
-                break;
-        }
-
+        Request request = this.request.build();
+        Clean();
         return request;
     }
 
+    private void Clean() {
+        this.request = new Request.Builder();
+    }
 }
